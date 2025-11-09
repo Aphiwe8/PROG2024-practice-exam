e389df26940eb6b0f0b7ee44192741369cead3ca
Aphiwe8 <st10493814@vcconnect.edu.za>
2025-11-09 21:49:48 +0200

    Add files via upload

---
+++ GUIDragAndDrop/src/main/java/com/mycompany/guidraganddrop/GUIDragAndDrop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guidraganddrop;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class GUIDragAndDrop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
